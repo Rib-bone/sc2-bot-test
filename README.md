86b96d44289bf043fcdea48b90536ad3c00d2724
Risto Turtiainen <risto.turtiainen@solita.fi>
2019-11-06 17:03:14 +0200

    run.py and new sc2 library

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # sc2-bot-test
 simple sc2 bot using voidray tactic
+
+Getting Started
+1. Install Starcraft 2 (system requirements)
+a. It’s free (up to a point) and available for Windows and macOS
+b. Linux users should be able to use Windows version with Wine. Headless Linux is also available.
+
+2. Download and install latest Ladder 2019 Season 3 map pack (notice the zip password and instructions)
+3. Install Python 3.7 if needed, preferably 64-bit
+a. Python 3.6 is okay and 3.8 won’t work afaik
+4. Clone the bot template and cd to its folder
+5. Create a virtual environment for Python (exact commands may vary slightly)
+a.
+python3 -m venv .myvenv
+b.
+source ./myvenv/bin/activate
+i.
+this needs to be called with every new console window
+6. Install python-sc2 library and other requirements (using the commands below)
+a.
+pip install -r requirements.txt
+b.
+pip install --upgrade --force-reinstall https://github.com/BurnySc2/python-sc2/archive/develop.zip
+7. Run the example bot
+a.
+python3 run.py
+8. If Starcraft 2 launches and it does not crash, you should be good to go!
--- sc2-bot-test.py
+++ sc2-bot-test.py
@@ -1,82 +1,66 @@
 import random
-
 import sc2
 from sc2 import Race, Difficulty, UnitTypeId
 from sc2.constants import AbilityId
 from sc2.ids.buff_id import BuffId
 from sc2.units import Units
 from sc2.player import Bot, Computer
-
 class ThreebaseVoidrayBot(sc2.BotAI):
-
     def select_target(self, state):
-        if self.known_enemy_structures.exists:
-            return random.choice(self.known_enemy_structures)
+        if self.enemy_structures.exists:
+            return random.choice(self.enemy_structures)
         return self.enemy_start_locations[0]
-    
     def get_random_pos(self):
         return [random.randrange(-5, 5), random.randrange(-5, 5)]
-
     async def defensive_patrol(self):
-        location = random.choice(self.units(UnitTypeId.NEXUS)).position.to2 + self.get_random_pos()
+        location = random.choice(self.townhalls).position.to2 + self.get_random_pos()
         for unit in self.defencive_force.idle:
-            await self.do(unit.attack(location))
-
+            self.do(unit.attack(location))
     async def on_step(self, iteration):
         if iteration == 0:
-            self.defencive_force: Units = Units([])
-            self.attack_force: Units = Units([])
+            self.defencive_force: Units = Units([], self)
+            self.attack_force: Units = Units([], self)
             await self.chat_send("(glhf)")
-
-        if not self.units(UnitTypeId.NEXUS).ready.exists:
+        if not self.townhalls.ready:
             for worker in self.workers:
-                await self.do(worker.attack(self.enemy_start_locations[0]))
+                self.do(worker.attack(self.enemy_start_locations[0]))
             return
         else:
-            nexus = self.units(UnitTypeId.NEXUS).ready.random
-
+            nexus = self.townhalls.ready.random
         if not nexus.has_buff(BuffId.CHRONOBOOSTENERGYCOST):
             abilities = await self.get_available_abilities(nexus)
             if AbilityId.EFFECT_CHRONOBOOSTENERGYCOST in abilities:
-                await self.do(nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, nexus))
-
+                self.do(nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, nexus))
         for idle_worker in self.workers.idle:
-            mf = self.state.mineral_field.closest_to(idle_worker)
-            await self.do(idle_worker.gather(mf))
-
-        if(iteration % 20 == 0):
+            mf = self.mineral_field.closest_to(idle_worker)
+            self.do(idle_worker.gather(mf))
+        if iteration % 20 == 0:
             for vr in self.attack_force:
-                await self.do(vr.attack(self.select_target(self.state)))
+                self.do(vr.attack(self.select_target(self.state)))
         if self.defencive_force.amount >= 10 and self.attack_force.amount < 10:
             self.attack_force = self.defencive_force
-            self.defencive_force = Units([])
+            self.defencive_force = Units([], self)
         else:
             self.defencive_force += (self.units(UnitTypeId.VOIDRAY).idle - self.attack_force - self.defencive_force)
             await self.defensive_patrol()
-
-        for a in self.units(UnitTypeId.ASSIMILATOR):
+        for a in self.structures(UnitTypeId.ASSIMILATOR):
             if a.assigned_harvesters < a.ideal_harvesters:
                 w = self.workers.closer_than(20, a)
                 if w.exists:
-                    await self.do(w.random.gather(a))
-
+                    self.do(w.random.gather(a))
         if self.supply_left < 4 and not self.already_pending(UnitTypeId.PYLON):
             if self.can_afford(UnitTypeId.PYLON):
                 await self.build(UnitTypeId.PYLON, near=nexus)
             return
-
-        if self.workers.amount < self.units(UnitTypeId.NEXUS).amount*15 and nexus.is_idle:
+        if self.workers.amount < self.townhalls.amount*15 and nexus.is_idle:
             if self.can_afford(UnitTypeId.PROBE):
-                await self.do(nexus.train(UnitTypeId.PROBE))
-
+                self.do(nexus.train(UnitTypeId.PROBE))
         elif not self.units(UnitTypeId.PYLON).exists and not self.already_pending(UnitTypeId.PYLON):
             if self.can_afford(UnitTypeId.PYLON):
                 await self.build(UnitTypeId.PYLON, near=nexus)
-
-        if self.units(UnitTypeId.NEXUS).amount < 3 and not self.already_pending(UnitTypeId.NEXUS):
+        if self.townhalls.amount < 3 and not self.already_pending(UnitTypeId.NEXUS):
             if self.can_afford(UnitTypeId.NEXUS):
                 await self.expand_now()
-
         if self.units(UnitTypeId.PYLON).ready.exists:
             pylon = self.units(UnitTypeId.PYLON).ready.random
             if self.units(UnitTypeId.GATEWAY).ready.exists:
@@ -86,35 +70,29 @@ class ThreebaseVoidrayBot(sc2.BotAI):
             else:
                 if self.can_afford(UnitTypeId.GATEWAY) and not self.already_pending(UnitTypeId.GATEWAY):
                     await self.build(UnitTypeId.GATEWAY, near=pylon)
-
-        for nexus in self.units(UnitTypeId.NEXUS).ready:
-            vgs = self.state.vespene_geyser.closer_than(20.0, nexus)
+        for nexus in self.townhalls.ready:
+            vgs = self.vespene_geyser.closer_than(20.0, nexus)
             for vg in vgs:
                 if not self.can_afford(UnitTypeId.ASSIMILATOR):
                     break
-
                 worker = self.select_build_worker(vg.position)
                 if worker is None:
                     break
-
-                if not self.units(UnitTypeId.ASSIMILATOR).closer_than(1.0, vg).exists:
-                    await self.do(worker.build(UnitTypeId.ASSIMILATOR, vg))
-
-        if self.units(UnitTypeId.PYLON).ready.exists and self.units(UnitTypeId.CYBERNETICSCORE).ready.exists:
+                if not self.gas_buildings or not self.gas_buildings.closer_than(1, vg):
+                    self.do(worker.build(UnitTypeId.ASSIMILATOR, vg), subtract_cost=True)
+                    self.do(worker.stop(queue=True))
+        if self.structures(UnitTypeId.PYLON).ready.exists and self.structures(UnitTypeId.CYBERNETICSCORE).ready.exists:
             pylon = self.units(UnitTypeId.PYLON).ready.random
-            if self.units(UnitTypeId.STARGATE).amount < 3 and not self.already_pending(UnitTypeId.STARGATE):
+            if self.structures(UnitTypeId.STARGATE).amount < 3 and not self.already_pending(UnitTypeId.STARGATE):
                 if self.can_afford(UnitTypeId.STARGATE):
                     await self.build(UnitTypeId.STARGATE, near=pylon)
-
-        for stargate in self.units(UnitTypeId.STARGATE).ready.idle:
+        for stargate in self.structures(UnitTypeId.STARGATE).ready.idle:
             if self.can_afford(UnitTypeId.VOIDRAY):
-                await self.do(stargate.train(UnitTypeId.VOIDRAY))
-
+                self.do(stargate.train(UnitTypeId.VOIDRAY))
 def main():
     sc2.run_game(sc2.maps.get("(2)CatalystLE"), [
         Bot(Race.Protoss, ThreebaseVoidrayBot()),
         Computer(Race.Terran, Difficulty.Easy)
     ], realtime=False)
-
 if __name__ == '__main__':
     main()
